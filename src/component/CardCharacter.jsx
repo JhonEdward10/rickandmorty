715228006ffb055cf4e252882a01743173e71d88
Jhon Edward <jhonedwardmendeztovar@gmail.com>
2022-06-15 22:46:32 -0500

    'actualizacion'

--- src/component/CardCharacter.jsx
+++ src/component/CardCharacter.jsx
@@ -5,12 +5,22 @@ const CardCharacter = ({resident}) => {
 
   const character = useCharacter(resident)
 
+  const circleChanged = {
+    Alive: 'green',
+    Dead: 'red',
+    unknown: 'gray'
+  }
+
   return (
     <article className='Card-Character'>
       <div>
       <img src={character?.image} alt="" />
       <h2><b>Name: </b>{character?.name}</h2>
-      <p><b>Status: </b>{character?.status}</p>
+      <p>
+        <b>Status: </b>{character?.status}
+        <span className="Circle grey red green" style={{ background: circleChanged[character?.status] }}
+        ></span>
+      </p>
       <p><b>Origin: </b>{character?.origin?.name}</p>
       <p><b>Episodes Where appear: </b>{character?.episode.length}</p>
       </div>
